260821e431775fdf343a4c302bd756d559a1a463
Irfandi D Vendy <d.vendy@yahoo.co.id>
2015-05-17 11:32:33 +0700

    First
    Sung!

---
+++ Assets/Meteor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Meteor : MonoBehaviour {
+
+	// gravity constant
+	public float g=9.8f;
+	Vector3 gravity;
+	void Update(){
+
+	}
+	
+	void FixedUpdate() {
+		// normalize axis
+		gravity = new Vector3 (
+			Input.acceleration.x,
+			0,
+			Input.acceleration.y
+			) * g;
+		
+		GetComponent<Rigidbody>().AddForce (gravity, ForceMode.Acceleration);
+	}
+}
---
+++ Assets/My_Camera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class My_Camera : MonoBehaviour {
+	public Transform target;
+	public int asd;
+	
+	void Update ()
+	{
+		//transform.LookAt(target);
+		transform.position = new Vector3(target.position.x ,transform.position.y , target.position.z+asd);
+	}
+}
